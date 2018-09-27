8bd73b4dabbe7b25c2d59d2b5a620a8ce27d8025
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-27 08:14:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/[aeiou]/) == nil? false : true
+  word.match(/[aeiouAEIOU]/) == nil? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
